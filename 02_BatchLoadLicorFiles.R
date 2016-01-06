5824fe2a9d61685514736e6d2fea11fdb770d084
davidjpmoore <davidjpmoore@arizona.edu>
2016-01-05 19:46:27 -0700

    Updated the batch file function to account for the revised file structure
    This should create lists of filenames and use PECAN to load the files into a giant list

--- 02_BatchLoadLicorFiles.R
+++ 02_BatchLoadLicorFiles.R
@@ -15,17 +15,60 @@ library(devtools)
 library(ggplot2)
 library(PEcAn.photosynthesis)
 
+################################################################################
+#####                       BEFORE YOU START                              ######
+#####                         READ THIS                                   ######
+################################################################################
+
+#https://github.com/PecanProject/pecan/blob/master/modules/photosynthesis/vignettes/ResponseCurves.Rmd
+
+################################################################################
+
+#
+#
+#
+# Seriously .... go back and read that!
 
 ## Get list of LI-COR 6400 file names (ASCII not xls)
 
 #this command looks for files *starting with* '2015-01-' within the folder "data" - the files adhere to 
 # a naming convention that let's the code pull out whether the file is an A/ci curve or an aq curve
 #
-filenames <- system.file("data", paste0("2015-01-",rep(1:6,each=2),c("aci","aq")), package = "PEcAn.photosynthesis")
+
+# ##Reading data files using PEcAn.photosynthesis function read.Licor
+# #Preferred way: load files to a list
+
+#Create a list of files from the "ACI" folder
+filepathACI="./data/ACI/"
+
+tempFilelistACI = list.files(filepathACI,pattern="2016-01")
+
+#Create a list of files from the "Light" folder
+filepathLight="./data/Light/"
+tempFilelistLIGHT = list.files(filepathLight,pattern="2016-01")
+
+#working directory on this computer
+cpath = getwd()
+
+#Creates ACIfiles object that is a list of the file locations
+midpathACI="/data/ACI/"
+ACIFiles = paste0(cpath,midpathACI,tempFilelistACI)
+
+#Creates Lightfiles object that is a list of the file locations
+midpathLight="/data/Light/"
+LightFiles = paste0(cpath,midpathLight,tempFilelistLIGHT)
 
 
-## Load files to a list
-master = lapply(filenames, read.Licor)
+# ##Perform QA/QC to get rid of outliers
+# #Make master list and apply read.Licor
+ACIMaster = lapply(ACIFiles, read.Licor)
+LIGHTMaster = lapply(LightFiles, read.Licor)
+
+
+
+#########################################
+###### MUST TROUBLE SHOOT BELOW #########
+#########################################
 
 # # The code below performs a set of interactive QA/QC checks on the LI-COR data that's been loaded. 
 # Because it's interactive it isn't run when this vignette is knit.
@@ -34,6 +77,7 @@ master = lapply(filenames, read.Licor)
 # want to run it first on just one file, rather than looping over all the files
 
 #this runs licor QC on a file - you click on outliers to remove them
+
 master[[1]] <- Licor.QC(master[[1]])
 
 #same process for all files
