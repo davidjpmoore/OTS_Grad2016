9171b040e488868f2a17618f519e3be680f51896
davidjpmoore <davidjpmoore@arizona.edu>
2016-01-07 14:46:41 -0700

    Added required libraries

--- 04_FitACI.R
+++ 04_FitACI.R
@@ -1,7 +1,12 @@
 ##### Fit ACI curves
+#Grab the following packages before you start
+library(devtools)
+library(PEcAn.photosynthesis)
 library(plantecophys)
 
 APari_plant1 = read.Licor("./data/Light/2016-01-05-ots-light-curve-plant-1")
 AACI_plant1 = read.Licor("./data/ACI/2016-01-05-ots-aci-curve-plant-1")
+
+
 fitaci(AACI_plant1)
 
